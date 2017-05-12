4753b0c2d7f78a2b6136e803ad29beb59cfb5d6d
Mayer Alexandra <mayer.ali@t-online.de>
2017-05-12 16:40:15 +0200

    Changed calculatin of x Data

--- Framework/PythonInterface/plugins/algorithms/LoadYaml.py
+++ Framework/PythonInterface/plugins/algorithms/LoadYaml.py
@@ -1,22 +1,17 @@
 from __future__ import (absolute_import, division, print_function)
 
 import mantid.simpleapi as sapi
-import os
-import sys
 import scipy.constants as const
 
-#from mantid.api import *
 from mantid.api import PythonAlgorithm, AlgorithmFactory, WorkspaceProperty, \
     FileProperty, FileAction
-from mantid.kernel import Direction, StringListValidator, DateAndTime
+from mantid.kernel import Direction, FloatBoundedValidator
 
 from yaml import load
 import math
 import numpy as np
-from yaml import CLoader as Loader, CDumper as Dumper
+from yaml import CLoader as Loader
 
-#!/usr/bin/env python
-# -*- coding: utf8 -*-
 
 class LoadYaml(PythonAlgorithm):
 
@@ -29,18 +24,29 @@ class LoadYaml(PythonAlgorithm):
 
     def PyInit(self):
 
-        self.declareProperty(FileProperty("Filename","",FileAction.Load, '.yaml'),"The File to load from")
+        self.declareProperty(FileProperty("Filename", "", FileAction.Load, '.yaml'), doc="The File to load from")
 
         self.declareProperty(WorkspaceProperty("OutputWorkspace",
                                                "", direction= Direction.Output),
                              doc="The name to use when saving the Workspace")
+
+        self.declareProperty(name='SourceSampleDistance', defaultValue=0.36325,
+                             validator=FloatBoundedValidator(lower=0.0),
+                             doc='Distance from chopper to sample in meters')
+
+        self.declareProperty(name='TofDelayFactor', defaultValue=1.0, validator=FloatBoundedValidator(lower=0.0))
+
+        self.declareProperty(name='ChannelWidthsFactor', defaultValue=1.0, validator=FloatBoundedValidator(lower=0.0))
+
         return
 
     def PyExec(self):
 
         workspace_name = self.getPropertyValue("OutputWorkspace")
         filename = self.getPropertyValue("Filename")
-        self.log().debug("Filename: " + filename)
+        L1 = float(self.getPropertyValue("SourceSampleDistance"))
+        tof_delay_factor = float(self.getPropertyValue("TofDelayFactor"))
+        channel_widths_factor = float(self.getPropertyValue("ChannelWidthsFactor"))
 
         file = open(filename)
 
@@ -59,26 +65,28 @@ class LoadYaml(PythonAlgorithm):
         slit_i       = setup['slit_i']
         xyz_coil     = setup['xyz_coil']
         tof          = setup['time_of_flight']
+
         tof_channels             = tof['number_of_channels']
-        tof_delay_duration       = tof['delay_duration']*10e6
-        tof_channel_duration_str = tof['channel_duration']
-        tof_channel_duration     = float(tof_channel_duration_str)*10e6
-        detector     = measurement['detectors'][0]
+        tof_delay_duration       = tof['delay_duration'] * tof_delay_factor
+        tof_channel_duration_str = tof['channel_duration'] * channel_widths_factor
+        tof_channel_duration     = float(tof_channel_duration_str)*1e+06
+
+        detector = measurement['detectors'][0]
 
         if measurement['setup']['polarization'] == 'off':
             pol = '0'
         else:
             pol = measurement['setup']['polarization']
 
-
         duration = history['duration_counted']
-        huber = orientation['rotation_angle']
-        flipp = str(flipper['setting']).upper()
+        huber    = orientation['rotation_angle']
+        flipp    = str(flipper['setting']).upper()
 
-        units['duration'] = "Second"
+        units['duration']   = "Second"
         units['wavelength'] = 'Angstrom'
-        units['angle'] = 'Degrees'
+        units['angle']      = 'Degrees'
 
+        wavelength = settings_for['incident_wavelength']
 
         sample_logs = [('run_title',                   measurement['unique_identifier'],    ''),
                        ('run_start',                   history['started'],                  ''),
@@ -89,7 +97,7 @@ class LoadYaml(PythonAlgorithm):
                        ('T2',                          temperature['T2']['mean'],           units['temperature']),
                        ('huber',                       huber,                               units['angle']),
                        ('omega',                       huber - duration,                    units['angle']),
-                       ('wavelength',                  settings_for['incident_wavelength'], units['wavelength']),
+                       ('wavelength',                  wavelength,                          units['wavelength']),
                        ('Ei',                          settings_for['incident_energy'],     units['energy']),
                        ('flipper',                     flipp,                               ''),
                        ('flipper_precession',          flipper['precession_current'],       units['current']),
@@ -104,9 +112,9 @@ class LoadYaml(PythonAlgorithm):
                        ('C_zb',                        xyz_coil['coil_zb_current'],         units['current']),
                        ('C_zt',                        xyz_coil['coil_zt_current'],         units['current']),
                        ('polarisation',                pol,                                 ''),
-                       ('tof_cannels',                 tof_channels*10e-6,                  ''),
+                       ('tof_channels',                tof_channels,                        ''),
                        ('tof_delay',                   tof_delay_duration,                  units['duration']),
-                       ('channel_width',               tof_channel_duration*10e-6,          units['duration']),
+                       ('channel_width',               tof_channel_duration,                units['duration']),
                        ('deterota',                    detector['angle_tube0'],             units['angle'])]
 
         log_names  = [item[0] for item in sample_logs]
@@ -122,46 +130,30 @@ class LoadYaml(PythonAlgorithm):
                 es.append(math.sqrt(y))
             e.append(es)
 
-        x = []
-        ts = []
-        t_last = tof_delay_duration
-        ts.append(t_last)
-        for i in range(1,tof_channels):
-            t = t_last + tof_channel_duration
-            ts.append(t)
-            t_last = t
+        v = const.h/(const.m_n*wavelength*1e-10)
+        tof1 = L1/v
+        x0 = tof1 + tof_delay_duration
 
-        x.append(ts[0] - (tof_channel_duration / 2))
-        for t in ts:
-            x.append(t+(tof_channel_duration/2))
-
-        dataX = np.array(x*ndet, np.float)
+        dataX = np.linspace(x0, x0 + tof_channels*tof_channel_duration, tof_channels+1)
         dataY = np.array([detector['counts'][i] for i in detector['counts']], np.float)
         dataE = np.array(e, np.float)
 
-
-
-        sapi.CreateWorkspace(OutputWorkspace = workspace_name, DataX = dataX, DataY = dataY,
-                             DataE = dataE, NSpec = ndet, UnitX="TOF")
+        sapi.CreateWorkspace(OutputWorkspace=workspace_name, DataX=dataX, DataY=dataY,
+                             DataE=dataE, NSpec=ndet, UnitX="TOF")
 
         out_ws = sapi.AnalysisDataService.retrieve(workspace_name)
-        self.log().debug("Workspace name: " + workspace_name)
 
         sapi.LoadInstrument(out_ws, InstrumentName=dataMap['instrument']['name'], RewriteSpectraMap=True)
 
         sapi.RotateInstrumentComponent(out_ws, "bank0", X=0, Y=1, Z=0, Angle=detector['angle_tube0'])
 
-
-        sapi.AddSampleLogMultiple(Workspace = out_ws, LogNames = log_names, LogValues = log_values, LogUnits = log_units)
+        sapi.AddSampleLogMultiple(Workspace=out_ws, LogNames=log_names,
+                                  LogValues=log_values, LogUnits = log_units)
 
         out_ws.setYUnit('Counts')
 
         self.setProperty("OutputWorkspace", out_ws)
 
-        self.log().debug('channels' + str(tof_channels))
-        self.log().debug('delay duration' + str(tof_channel_duration))
-        self.log().debug('channel duration' + str(tof_channel_duration_str))
-        self.log().debug(str(const.eV))
-
 ##########################################################################################
+
 AlgorithmFactory.subscribe(LoadYaml)
